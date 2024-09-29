7d0d3ec7249cec97367d37a01f2e71091441d4c1
Saeed Alipour <saeedalipourdev@gmail.com>
2024-09-29 22:43:04 +0330

    Replaced month and year dropdown arrows with SVG format arrows

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -177,7 +177,7 @@ function makeMonthDropdownWork() {
   optionsContainer.querySelectorAll('div')
     .forEach((option) => {
       option.addEventListener('click', () => {
-        document.querySelector('.month-selector').innerHTML = option.innerHTML + ' &#x2BC6;';
+        document.querySelector('.month-selector').innerHTML = option.innerHTML + `<svg class="w-[20px] h-[20px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 10 4 4 4-4"/></svg>`;
         optionsContainer.style.display = 'none';
       });
     });
@@ -207,7 +207,7 @@ function makeYearDropdownWork() {
   optionsContainer.querySelectorAll('div')
     .forEach((option) => {
       option.addEventListener('click', () => {
-        document.querySelector('.year-selector').innerHTML = option.innerHTML + ' &#x2BC6;';
+        document.querySelector('.year-selector').innerHTML = option.innerHTML + `<svg class="w-[20px] h-[20px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 10 4 4 4-4"/></svg>`;
         optionsContainer.style.display = 'none';
       });
     });
